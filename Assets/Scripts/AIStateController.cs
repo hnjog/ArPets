e9dfa3c725f1c4a06f1dad4dd81fcf7094d25636
hnjog <43630972+hnjog@users.noreply.github.com>
2019-09-08 15:08:09 +0900

    190908
    상태 스크립트 작성중, 다시 자이로스코프 사용 스크립트(그로 인한 위키튜드 에셋 삭제)

--- Assets/Scripts/AIStateController.cs
+++ Assets/Scripts/AIStateController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 // 공놀이, 먹이 주기 - 오브젝트 풀로? 
 // 해당 조건이 끝나면 다시 기본 상태로 한 번 돌아옴.
 // UI 특정 메뉴 선택 시 먹이주기, 공놀이 작동
+// 만복도, 행복도 관련 작업 중
+
 
 public class AIStateController : AI
 {
@@ -16,29 +18,90 @@ public class AIStateController : AI
     [SerializeField]
     Renderer veluga_SRenderer;                             // 벨루가 표정 렌더러
 
-    [Header("공놀이, 먹이주기 프리팹들")]
+    [Header("공놀이, 먹이주기관련 변수")]
     [SerializeField]
-    GameObject ball;
+    GameObject ballPrefab;                                 // 공 프리팹
     [SerializeField]
-    GameObject food;
+    GameObject foodPrefab;                                 // 먹이 프리팹
+    bool isDoingBall = false;                              // 공놀이 중
+    bool isDoingFeed = false;                              // 먹이 먹는 중
 
-    // 먹이주기, UI 메뉴에서 사용될 예정
-    void Feed()
+
+    [Header("행복도 비교용")]
+    int checkHappy = 0;                                    // 행복도 비교용(기존의 행복도)
+    bool isHappy = false;                                  // 행복함 (기존의 행복도 보다 변화한 행복도가 높음)
+    bool isSad = false;                                    // 안 행복함(기존의 행복도 보다 변화한 행복도가 낮음)
+
+    private void Start()
     {
-        veluga_State = Chara_State.state_Eat;
+        //깜짝 놀람 애니메이션 & 인사 하는 것
+        checkHappy = happiness;
+        StartCoroutine(StateChange());
+        StartCoroutine(HappyChecker());
     }
 
-    // 공놀이
-    void Ball()
+    // 공놀이 선택
+    void UIBall()
     {
         veluga_State = Chara_State.state_Ball;
     }
 
+    // 먹이주기, UI 메뉴에서 사용될 예정
+    void UIFeed()
+    {
+        veluga_State = Chara_State.state_Eat;
+    }
+
+
     // 상태 변화
     IEnumerator StateChange()
     {
+        while(true)
+        {
+            // 공놀이나, 먹이주기 상태가 아닐때!
+            if (!isDoingBall || !isDoingFeed)
+            {
+                // 공놀이
+                if (veluga_State == Chara_State.state_Ball)
+                {
+                    PlayBall();
+                }
+                // 먹이주기
+                else if (veluga_State == Chara_State.state_Eat)
+                {
+                    PlayFeed();
+                }
+                // 행복도 상승
+                else if (isHappy)
+                {
+                    StartCoroutine(State_Happy());
+                }
+                // 행복도 하락
+                else if (isSad)
+                {
+                    StartCoroutine(State_Sad());
+                }
+                // 만복도 40이하
+                else if (foodPoint <= 40)
+                {
+                    StartCoroutine(State_Hunger());
+                }
 
-        yield return null;
+            }
+            yield return null;
+        }
+    }
+
+    // 공놀이 메서드
+    void PlayBall()
+    {
+
+    }
+
+    // 먹이주기 메서드
+    void PlayFeed()
+    {
+
     }
 
     // 배고픔 상태 일시 사용될 코루틴
@@ -55,6 +118,7 @@ public class AIStateController : AI
         // 관련 애니메이션
         yield return new WaitForSeconds(3f);
         veluga_State = Chara_State.state_Idle;
+        isHappy = false;
     }
 
     // 슬픔 상태 일시 사용될 코루틴
@@ -63,5 +127,47 @@ public class AIStateController : AI
         // 관련 애니메이션
         yield return new WaitForSeconds(3f);
         veluga_State = Chara_State.state_Idle;
+        isSad = false;
+    }
+
+    // 행복도 변화 체크를 위해 사용 
+    IEnumerator HappyChecker()
+    {
+        while(true)
+        {
+            if (checkHappy < happiness)
+            {
+                isHappy = true;
+                checkHappy = happiness;
+            }
+            else if (checkHappy > happiness)
+            {
+                isSad = true;
+                checkHappy = happiness;
+            }
+            yield return null;
+        }
+    }
+
+    // 배고픔 애니메이션 관련
+    IEnumerator State_Hunger()
+    {
+        // 30% 확률
+        if (Random.value <= 0.3)
+        {
+            //배고픔 애니메이션 실행
+            yield return new WaitForSecondsRealtime(3f);
+        }
+        // 다시 실행할 때 갑자기 꺼지는 일이 없도록
+        else
+            yield return null;
+    }
+
+    // 1분에 만복도 10 감소
+    IEnumerator HungerIsComing()
+    {
+        
+        yield return new WaitForSecondsRealtime(60f);
+        foodPoint -= 10;
     }
 }
